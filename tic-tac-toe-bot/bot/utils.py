0880332420bc40c64cef2398c130660c7d2faa3a
PacoAcun <103969393+PacoAcun@users.noreply.github.com>
2022-05-15 01:11:46 -0600

    finish bot

--- tic-tac-toe-bot/bot/utils.py
+++ tic-tac-toe-bot/bot/utils.py
@@ -76,140 +76,332 @@ def decide_move(board: list, player_id: str) -> [int, int]:
         column = 0
         return[row, column]
 
-    def win(board,r ,c):
-        
+    if player_id == "O" and board[1][1] == "-":
+        row = 1
+        column = 1
+        return[row, column]
+
+    def win1(board,r ,c):
+
         r = 0
         c = 1
 
-        for i in range(len(board)):
-            
-            if c >= 3:
-                c = 1
+        while r < 2:
+            for i in range(len(board)):
+                if c >= 3:
+                    c = 1
+                    
+                if player_id == board[r][i] == board[c][i]:
+                    if r == 0:
+                        if i == 0 and board[2][0] == "-":
+                            row = 2
+                            column = 0
+                            r = 2
+                            return [row, column]
+                        if i == 1 and board[2][1] == "-":
+                            row = 2
+                            column = 1
+                            r = 2
+                            return [row, column]
+                        if i == 2 and board[2][2] == "-":
+                            row = 2
+                            column = 2
+                            r = 2
+                            return [row, column]
                 
-            if "X" == board[r][i] == board[c][i] or "O" == board[r][i] == board[c][i]:
-                if r == 0:
-                    if i == 0 and board[2][0] == "-":
-                        row = 2
-                        column = 0
-                        return [row, column]
-                    if i == 1 and board[2][1] == "-":
-                        row = 2
-                        column = 1
-                        return [row, column]
-                    if i == 2 and board[2][2] == "-":
-                        row = 2
-                        column = 2
-                        return [row, column]
-            
-                if r == 1:
-                    if i == 0 and board[0][0] == "-":
-                        row = 0
-                        column = 0
-                        return [row, column]
-                    if i == 1 and board[0][1] == "-":
-                        row = 0
-                        column = 1
-                        return [row, column]
-                    if i == 2 and board[0][2] == "-":
-                        row = 0
-                        column = 2
-                        return [row, column]
+                    if r == 1:
+                        if i == 0 and board[0][0] == "-":
+                            row = 0
+                            column = 0
+                            r = 2
+                            return [row, column]
+                        if i == 1 and board[0][1] == "-":
+                            row = 0
+                            column = 1
+                            r = 2
+                            return [row, column]
+                        if i == 2 and board[0][2] == "-":
+                            row = 0
+                            column = 2
+                            r = 2
+                            return [row, column]
 
-            if "X" == board[i][r] == board[i][c] or "O" == board[i][r] == board[i][c]:
-                if r == 0:
-                    if i == 0 and board[0][2] == "-":
-                        row = 0
-                        column = 2
+                if player_id == board[i][r] == board[i][c]:
+                    if r == 0:
+                        if i == 0 and board[0][2] == "-":
+                            row = 0
+                            column = 2
+                            r = 2
+                            return [row, column]
+                        if i == 1 and board[1][2] == "-":
+                            row = 1
+                            column = 2
+                            r = 2
+                            return [row, column]
+                        if i == 2 and board[2][2] == "-":
+                            row = 2
+                            column = 2
+                            r = 2
+                            return [row, column]
+
+                    if r == 1:
+                        if i == 0 and board[2][0] == "-":
+                            row = 0
+                            column = 0
+                            r = 2
+                            return [row, column]
+                        if i == 1 and board[1][0] == "-":
+                            row = 1
+                            column = 0
+                            r = 2
+                            return [row, column]
+                        if i == 2 and board[2][0] == "-":
+                            row = 2
+                            column = 0
+                            r = 2
+                            return [row, column]
+
+                if player_id == board[0][i] == board[2][i]:
+                    if i == 0 and board[1][0] == "-":
+                        row = 1
+                        column = 0
+                        r = 2
                         return [row, column]
-                    if i == 1 and board[1][2] == "-":
+                    if i == 1 and board[1][1] == "-":
+                        row = 1
+                        column = 1
+                        r = 2
+                        return [row, column]
+                    if i == 2 and board[1][2] == "-":
                         row = 1
                         column = 2
-                        return [row, column]
-                    if i == 2 and board[2][2] == "-":
-                        row = 2
-                        column = 2
+                        r = 2
                         return [row, column]
 
-                if r == 1:
-                    if i == 0 and board[2][0] == "-":
+                if player_id == board[i][0] == board[i][2]:
+                    print("Esta")
+                    if i == 0 and board[0][1] == "-":
                         row = 0
-                        column = 0
+                        column = 1
+                        r = 2
                         return [row, column]
-                    if i == 1 and board[1][0] == "-":
+                    if i == 1 and board[1][1] == "-":
                         row = 1
-                        column = 0
+                        column = 1
+                        r = 2
                         return [row, column]
-                    if i == 2 and board[2][0] == "-":
+                    if i == 2 and board[2][1] == "-":
                         row = 2
-                        column = 0
+                        column = 1
+                        r = 2
+                        print("ret")
                         return [row, column]
 
-            if "X" == board[0][i] == board[2][i] or "O" == board[0][i] == board[2][i]:
-                if i == 0 and board[1][0] == "-":
-                    row = 1
-                    column = 0
-                    return [row, column]
-                if i == 1 and board[1][1] == "-":
+
+                if player_id == board[0][0] == board[2][2] and board[1][1] == "-" or player_id == board[0][2] == board[2][0] and board[1][1] == "-":
                     row = 1
                     column = 1
-                    return [row, column]
-                if i == 2 and board[1][2] == "-":
-                    row = 1
-                    column = 2
+                    r = 2
                     return [row, column]
 
-            if "X" == board[i][0] == board[i][2] or "O" == board[i][0] == board[i][2]:
-                if i == 0 and board[0][1] == "-":
-                    row = 0
-                    column = 1
-                    return [row, column]
-                if i == 1 and board[1][1] == "-":
-                    row = 1
-                    column = 1
-                    return [row, column]
-                if i == 2 and board[2][2] == "-":
+                if player_id == board[0][0] == board[1][1] and board[2][2] == "-":
                     row = 2
                     column = 2
+                    r = 2
                     return [row, column]
 
+                if player_id == board[0][2] == board[1][1] and board[2][0] == "-":
+                    row = 2
+                    column = 0
+                    r = 2
+                    return [row, column]
 
-            if "X" == board[0][0] == board[2][2] and board[1][1] == "-" or "X" == board[0][2] == board[2][0] and board[1][1] == "-" or "O" == board[0][0] == board[2][2] and board[1][1] == "-" or "O" == board[0][2] == board[2][0] and board[1][1] == "-":
-                row = 1
-                column = 1
-                return [row, column]
+                if player_id == board[2][0] == board[1][1] and board[0][2] == "-":
+                    row = 0
+                    column = 2
+                    r = 2
+                    return [row, column]
 
-            if "X" == board[0][0] == board[1][1] and board[2][2] == "-" or "O" == board[0][0] == board[1][1] and board[2][2] == "-":
-                row = 2
-                column = 2
-                return [row, column]
+                if player_id == board[2][2] == board[1][1] and board[0][0] == "-":
+                    row = 0
+                    column = 0
+                    r = 2
+                    return [row, column]
 
-            if "X" == board[0][2] == board[1][1] and board[2][0] == "-" or "O" == board[0][2] == board[1][1] and board[2][0] == "-":
-                row = 2
-                column = 0
-                return [row, column]
+  
+            r += 1
+            c += 1
 
-            if "X" == board[2][0] == board[1][1] and board[0][2] == "-" or "O" == board[2][0] == board[1][1] and board[0][2] == "-":
-                row = 0
-                column = 2
-                return [row, column]
 
-            if "X" == board[2][2] == board[1][1] and board[0][0] == "-" or "O" == board[2][2] == board[1][1] and board[0][0] == "-":
-                row = 0
-                column = 0
-                return [row, column]
+        return False
 
-            if r < 2:    
-                r += 1
-                c += 1
+    def win(board,r ,c):
 
-            else:
-                return False
+        r = 0
+        c = 1
+        
+        while r < 2:
+            for i in range(len(board)):
+
+                if c >= 3:
+                    c = 1
+                    
+                if "X" == board[r][i] == board[c][i] or "O" == board[r][i] == board[c][i]:
+                    if r == 0:
+                        if i == 0 and board[2][0] == "-":
+                            row = 2
+                            column = 0
+                            r = 2
+                            return [row, column]
+                        if i == 1 and board[2][1] == "-":
+                            row = 2
+                            column = 1
+                            r = 2
+                            return [row, column]
+                        if i == 2 and board[2][2] == "-":
+                            row = 2
+                            column = 2
+                            r = 2
+                            return [row, column]
+
+                    if r == 1:
+                        if i == 0 and board[0][0] == "-":
+                            row = 0
+                            column = 0
+                            r = 2
+                            return [row, column]
+                        if i == 1 and board[0][1] == "-":
+                            row = 0
+                            column = 1
+                            r = 2
+                            return [row, column]
+                        if i == 2 and board[0][2] == "-":
+                            row = 0
+                            column = 2
+                            r = 2
+                            return [row, column]
+
+                if "X" == board[i][r] == board[i][c] or "O" == board[i][r] == board[i][c]:
+                    if r == 0:
+                        if i == 0 and board[0][2] == "-":
+                            row = 0
+                            column = 2
+                            r = 2
+                            return [row, column]
+                        if i == 1 and board[1][2] == "-":
+                            row = 1
+                            column = 2
+                            r = 2
+                            return [row, column]
+                        if i == 2 and board[2][2] == "-":
+                            row = 2
+                            column = 2
+                            return [row, column]
+
+                    if r == 1:
+                        if i == 0 and board[2][0] == "-":
+                            row = 0
+                            column = 0
+                            r = 2
+                            return [row, column]
+                        if i == 1 and board[1][0] == "-":
+                            row = 1
+                            column = 0
+                            r = 2
+                            return [row, column]
+                        if i == 2 and board[2][0] == "-":
+                            row = 2
+                            column = 0
+                            r = 2
+                            return [row, column]
+
+                if "X" == board[0][i] == board[2][i] or "O" == board[0][i] == board[2][i]:
+                    if i == 0 and board[1][0] == "-":
+                        row = 1
+                        column = 0
+                        r = 2
+                        return [row, column]
+                    if i == 1 and board[1][1] == "-":
+                        row = 1
+                        column = 1
+                        r = 2
+                        return [row, column]
+                    if i == 2 and board[1][2] == "-":
+                        row = 1
+                        column = 2
+                        r = 2
+                        return [row, column]
+
+                if "X" == board[i][0] == board[i][2] or "O" == board[i][0] == board[i][2]:
+                    if i == 0 and board[0][1] == "-":
+                        row = 0
+                        column = 1
+                        r = 2
+                        return [row, column]
+                    if i == 1 and board[1][1] == "-":
+                        row = 1
+                        column = 1
+                        r = 2
+                        return [row, column]
+                    if i == 2 and board[2][1] == "-":
+                        row = 2
+                        column = 1
+                        r = 2
+                        return [row, column]
+
+
+                if "X" == board[0][0] == board[2][2] and board[1][1] == "-" or "X" == board[0][2] == board[2][0] and board[1][1] == "-" or "O" == board[0][0] == board[2][2] and board[1][1] == "-" or "O" == board[0][2] == board[2][0] and board[1][1] == "-":
+                    row = 1
+                    column = 1
+                    r = 2
+                    return [row, column]
+
+                if "X" == board[0][0] == board[1][1] and board[2][2] == "-" or "O" == board[0][0] == board[1][1] and board[2][2] == "-":
+                    row = 2
+                    column = 2
+                    r = 2
+                    return [row, column]
+
+                if "X" == board[0][2] == board[1][1] and board[2][0] == "-" or "O" == board[0][2] == board[1][1] and board[2][0] == "-":
+                    row = 2
+                    column = 0
+                    r = 2
+                    return [row, column]
+
+                if "X" == board[2][0] == board[1][1] and board[0][2] == "-" or "O" == board[2][0] == board[1][1] and board[0][2] == "-":
+                    row = 0
+                    column = 2
+                    r = 2
+                    return [row, column]
+
+                if "X" == board[2][2] == board[1][1] and board[0][0] == "-" or "O" == board[2][2] == board[1][1] and board[0][0] == "-":
+                    row = 0
+                    column = 0
+                    r = 2
+                    return [row, column]
+
+            r += 1
+            c += 1
+
+
+        return False
 
     r = 0
     c = 1
-    win(board, r, c)
+    win1(board, r, c)
+
+    if win1(board, r, c) == False:
+        win(board, r, c)
+
+    if win1(board, r, c) != False:
+        return win1(board, r, c)
 
     if win(board, r, c) == False:
+        if player_id == "O" and board[0][0] == board[2][2] == "X" or player_id == "O" and board[0][2] == board[2][0] == "X":
+            row = 1
+            column = 2
+            return[row, column]
+
         if board[0][0] == "-":
             row = 0
             column = 0
